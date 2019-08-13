ccdea504d1f0d5f0e397e43757efe478c61f6e0c
SimonCrA <csimonr28@gmail.com>
2019-08-13 14:06:15 -0400

    encapsulamiento de las credenciales de mongoDB

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://mariameama:yTCN8XbeOji6mMMC@cluster0-qql62.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
