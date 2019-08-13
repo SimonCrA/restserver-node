6a89363aaf50f370f9d88984ea57cb4b4210200d
SimonCrA <csimonr28@gmail.com>
2019-08-13 13:07:27 -0400

    restServer-Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,26 @@
 
-
 //=================
 //Port
 //=================
 
 process.env.PORT = process.env.PORT || 3000;
+
+//=================
+//Entorno
+//=================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=================
+//Base de Datos
+//=================
+
+let urlDB;
+
+if( process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = 'mongodb+srv://mariameama:yTCN8XbeOji6mMMC@cluster0-qql62.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,46 +1,27 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
+ 
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // parse application/json
 app.use(bodyParser.json())
+//importación del archivo usuario en routes
+app.use ( require('./routes/usuario') );
  
-app.get('/usuario', function (req, res) {
-  res.json('get Usuario')
-})
+mongoose.set('useCreateIndex', true)
 
-app.post('/usuario', function (req, res) {
-    
-    let body = req.body;
-    if (res.body === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-})
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, resp) => {
 
-app.put('/usuario/:id', function (req, res) {
-    
-    let id = req.params.id;
+    if (err) throw err;
 
-    res.json({
-        id
-    })
-})
+    console.log('base de datos ONLINE');
 
-app.delete('/usuario', function (req, res) {
-    res.json('delete Usuario')
-})
+});
 
 app.listen(process.env.PORT, () => {
     console.log('escuchando el puerto, 3000');
